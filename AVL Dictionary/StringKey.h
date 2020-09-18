227663ccc4eda50304839554c6cf1b93cda3913e
liorsl1 <liorsl36@gmail.com>
2020-09-18 10:30:18 +0300

    Comments update

--- AVL Dictionary/StringKey.h
+++ AVL Dictionary/StringKey.h
@@ -9,12 +9,13 @@ private:
 	string FirstName;
 	string LastName;
 public:
-	// theta(1) for every function
+	// theta(1) for every function, except for Compare.
 	StringKey();
 	StringKey(string first, string last);
 	StringKey(const StringKey& key);
 	~StringKey();
 	StringKey* Read();
+	// theta(n) - where n is the length of the string.
 	int Compare(StringKey* sKey);
 	void toString();
 };
